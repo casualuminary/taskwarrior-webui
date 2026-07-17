61725e4baa69419f6358889932e3d957aaa10cd7
Bharti <casualuminary@users.noreply.github.com>
2026-07-17 20:30:02 +0200

    rerouting to index page in case of 404

--- taskwarrior-webui/frontend/layouts/error.vue
+++ taskwarrior-webui/frontend/layouts/error.vue
@@ -1,40 +1,13 @@
-<template>
-	<v-app>
-		<h2 class="headline">
-			{{ error.statusCode }}
-		</h2>
-		<div>
-			{{ error.message }}
-		</div>
-		<NuxtLink to="/">
-			Home page
-		</NuxtLink>
-	</v-app>
-</template>
-
-<script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api';
-import { NuxtError } from '@nuxt/types';
-
-export default defineComponent({
-	data () {
-		return {
-			pageNotFound: '404 Not Found',
-			otherError: 'An error occurred'
-		};
-	},
-
-	props: {
-		error: {
-			type: Object as () => NuxtError,
-			required: true
+<script>
+export default {
+	mounted() {
+		if (process.client) {
+			this.$router.replace('/')
 		}
-	},
-
-	setup(props) {
-		return {
-			error: props.error
-		};
 	}
-});
+}
 </script>
+
+<template>
+	<div />
+</template>
